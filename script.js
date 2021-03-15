64f5d5792af7525a97fa00250d3e794e5ba528dd
Chaos <rizwannafees13@gmail.com>
2021-03-14 22:07:47 -0400

    Half way done

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Global Scope ------------------------------------------------------------------------------------------------------------------------------------------
+const html = document.querySelector('html')
 const container = document.querySelector('.container2');
 const drawingColor = document.querySelector('#color');
+const bgColor = document.querySelector('.bg-color')
 let gridSize = document.querySelector('#range');
-let gridBox = Number(gridSize.value);
+const borderColor = document.querySelector('.borderColor');
 // Creating Grid -----------------------------------------------------------------------------------------------------------------------------------------
-function color(gridBox) {
+function creatingGrid(gridBox) {
 for (let i=0; i < gridBox * gridBox; i++) {
     let grid = document.createElement('div');
     grid.classList.add('grid');
@@ -14,10 +16,50 @@ for (let i=0; i < gridBox * gridBox; i++) {
     grid.addEventListener('mouseover', ()=> {
         grid.style.backgroundColor = drawingColor.value;
     });
+    bgColor.addEventListener('input', ()=> {
+        grid.style.backgroundColor = bgColor.value;
+    });
+    borderColor.addEventListener('input',()=> {
+        grid.style.border = `2px solid ${borderColor.value}`
+    })
 }
 }
-color(gridBox)
-// color(gridBox);
+creatingGrid(gridSize.value);
+
+// Changing Grid Size -------------------------------------------------------------------------------------------------------------------------------------
+function clear() {
+    const clearGrid = Array.from(container.childNodes);
+    clearGrid.forEach(gridItems => {
+      container.removeChild(gridItems);
+    });
+  }
 gridSize.addEventListener('change', ()=> {
-    color(gridBox);
+    clear();
+    creatingGrid(gridSize.value);
+    let gridSizeIndicator = document.querySelector('.gridSizeIndicator');
+    gridSizeIndicator.textContent = `${gridSize.value} x ${gridSize.value}` 
 })
+// Cursor --------------------------------------------------------------------------------------------------------------------------------------
+const cursor = document.querySelector('.cursor')
+const cursorChange = Array.from(cursor.childNodes);
+function changeCursor() {
+    cursorChange[1].addEventListener('click', ()=> {
+        html.style.cursor = 'url(\'images/cursor1.png\'), pointer';
+    });
+    cursorChange[3].addEventListener('click', ()=> {
+        html.style.cursor = 'url(\'images/cursor2.png\'), pointer';
+    });
+    cursorChange[5].addEventListener('click', ()=> {
+        html.style.cursor = 'url(\'images/cursor3.png\'), pointer';
+    });
+    cursorChange[7].addEventListener('click', ()=> {
+        html.style.cursor = 'url(\'images/cursor4.png\'), pointer';
+    });
+    cursorChange[9].addEventListener('click', ()=> {
+        html.style.cursor = 'url(\'images/cursor5.png\'), pointer';
+    });
+    cursorChange[11].addEventListener('click', ()=> {
+        html.style.cursor = 'url(\'images/cursor6.png\'), pointer';
+    });
+}
+changeCursor();
