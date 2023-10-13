1f62c157ebf3a33b0f9a95efb793165e6cca27b0
Real-Chaos <rizwannafees13@gmail.com>
2023-10-13 16:53:22 -0400

    refactored code before adding more functionaly

--- main.js
+++ main.js
@@ -3,43 +3,37 @@ const grid = document.querySelector('.grid')
 const gridOptions = document.querySelectorAll('.grid-options button')
 const gridSize = document.querySelector('.grid-size')
 const sizeIndicator = document.querySelector('.size-indicator')
+const colorSelected = document.querySelector('.color-selected')
 
-gridSize.addEventListener('input', () => {
-  sizeIndicator.textContent = `${gridSize.value} x ${gridSize.value}`
-  createGrid()
-})
-
-
-
-gridOptions.forEach((option) => {
-  option.addEventListener('click', (e) => {
-    gridOptions.forEach(op => {
-      op.style.backgroundColor = 'transparent'
-      op.style.color = '#333'
+const handlingModes = () => {
+  gridOptions.forEach((option) => {
+    option.addEventListener('click', (e) => {
+      gridOptions.forEach((op) => {
+        op.style.backgroundColor = 'transparent'
+        op.style.color = '#333'
+      })
+      e.target.style.color = 'white'
+      e.target.style.backgroundColor = '#333'
+      mode = e.target.className
     })
-    e.target.style.color = 'white'
-    e.target.style.backgroundColor = '#333'
-    mode = e.target.className
-    console.log(mode)
   })
-})
+}
 
 const colorMode = () => {
-  if(mode === 'color') {
-    const colorSelected = document.querySelector('.color-selected')
-    colorSelected.addEventListener('change', (e) => {
-      
-    }) 
+  if (mode === 'color') {
+    colorSelected.addEventListener('change', (e) => {})
   }
 }
 
-
 const createGrid = () => {
-  grid.innerHTML=""
-  console.log(grid)
-  if(grid.innerHTML==="") {
-    for(let i = 0; i <= gridSize.value * gridSize.value; i++) {
-    
+  grid.innerHTML = ''
+  sizeIndicator.textContent = `${gridSize.value} x ${gridSize.value}`
+  gridSize.addEventListener('input', () => {
+    sizeIndicator.textContent = `${gridSize.value} x ${gridSize.value}`
+    createGrid()
+  })
+  if (grid.innerHTML === '') {
+    for (let i = 0; i <= gridSize.value * gridSize.value; i++) {
       const div = document.createElement('div')
       grid.style.gridTemplateColumns = `repeat(${gridSize.value}, 1fr)`
       grid.style.gridTemplateRows = `repeat(${gridSize.value}, 1fr)`
@@ -50,3 +44,6 @@ const createGrid = () => {
 }
 
 colorMode()
+handlingModes()
+createGrid()
+
